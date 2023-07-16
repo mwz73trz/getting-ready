7158f20d765cc42ecb1dc5f428e45eb4903bbc38
mwz73trz <michaelzura@bellsouth.net>
2023-07-16 18:14:01 -0400

    completed

---
+++ sum.test.js
@@ -0,0 +1,7 @@
+var sumAdd = require("./sum");
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sumAdd.sum(1, 2)).toBe(3);
+  expect(sumAdd.sum(2, 1)).toBe(3);
+  expect(sumAdd.sum(5, 2)).toBe(7);
+});
